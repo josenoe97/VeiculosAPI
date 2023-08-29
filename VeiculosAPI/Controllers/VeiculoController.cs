9a3a1089c498e0f1334ce85edc071a56edf7a960
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-08-29 09:07:57 -0300

    Criação do controller  VeiculoController e Model Veiculo

---
+++ VeiculosAPI/Controllers/VeiculoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VeiculosAPI.Models;
+
+namespace VeiculosAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class VeiculoController : ControllerBase
+{
+    private static List<Veiculo> veiculos = new List<Veiculo>();
+
+    [HttpPost]
+    public void AdicionaVeiculo([FromBody] Veiculo veiculo) //From body vendo do corpo da requisição
+    {
+        veiculos.Add(veiculo);
+        Console.WriteLine(veiculo.Marca);
+        Console.WriteLine(veiculo.Modelo);
+    }
+}
---
+++ VeiculosAPI/Models/Veiculo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VeiculosAPI.Models;
+
+public class Veiculo
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Marca { get; set; }
+
+    [Required]
+    public string Modelo { get; set; }
+
+    [Required]
+    [DisplayFormat(DataFormatString = "{0:yyyy}")]
+    public DateTime Ano { get; set; }
+
+}
